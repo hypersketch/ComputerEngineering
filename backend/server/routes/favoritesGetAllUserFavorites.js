c13b2cc38ef022a430afc396dfa7b2d7188f29d7
Josue Salazar <josue0sala@gmail.com>
2024-03-07 12:21:41 -0500

    added error handling to getAllFavorites

--- backend/server/routes/favoritesGetAllUserFavorites.js
+++ backend/server/routes/favoritesGetAllUserFavorites.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const favoritesModel = require("../models/favoritesModel")
+const newUserModel = require("../models/userModel")
 
 router.get('/:username', async (req, res) => {
     const username = req.params.username
     const fav = await favoritesModel.find({username: username})
-    
+    const user = await newUserModel.findOne({username: username})
+    if (!user){
+        res.status(408).send("UserID not found")
+        
+    }else{
     return res.json(fav)
+    }
     })
   module.exports = router;
